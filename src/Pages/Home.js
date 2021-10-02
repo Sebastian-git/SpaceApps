a00740f34c5758ae3409307046354405aaf5cccc
Sebastian Cevallos <sebastian01cevallos@gmail.com>
2021-10-02 10:38:21 -0700

    First commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react"
+import "./Home.css"
+
+class Home extends Component {
+
+  state = {
+      
+  }
+
+  render() {
+    return (
+    <React.Fragment>
+        <p> Hello world </p>
+    </React.Fragment>
+    )
+  }
+}
+
+export default Home;
