e6f3a8cdf1c57c466368edd2b34832b1bc1f3c82
Tyler Truong <tylertehasian@gmail.com>
2021-10-03 17:21:45 -0700

    Title Changes

--- src/Components/Sunshine.js
+++ src/Components/Sunshine.js
@@ -4,6 +4,8 @@ import SunShineGraph from "../Components/SunshineGraph.js"
 
 import "./Sunshine.css"
 
+import sunshineImage from "../img/sunshinePlain.png"
+
 class Sunshine extends Component {
 
     state = {
@@ -23,6 +25,7 @@ class Sunshine extends Component {
           <React.Fragment>
             <div id="contentTop">
                 <p id="contentTitle"> Available Sunshine </p>
+                <img id="contentImg" src={sunshineImage} align="right"></img>
             </div>
             <div id="graph">
                 <SunShineGraph prevTemperature={this.state.temperature} updateTemperature={this.updateTemperature} />
